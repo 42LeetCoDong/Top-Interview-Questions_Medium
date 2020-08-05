2de640b7bdb63d7df2137f8e7cba9dbccdd52191
seongwpa <seongwpa@student.42Seoul.kr>
2020-08-05 13:25:01 +0900

    seongwpa 8/5 modified

--- 01. Array and Strings/03. Group Anagrams/Group-Anagrams_seongwpa.js
+++ 01. Array and Strings/03. Group Anagrams/Group-Anagrams_seongwpa.js
@@ -1,3 +1,4 @@
+/*
 var groupAnagrams = function(strs) {
     let sstr = [];
     for (let i = 0; i < strs.length; i++)
@@ -17,6 +18,28 @@ var groupAnagrams = function(strs) {
         }
         tmp.sort();
         res.push(tmp);
+    }
+    return res;
+};
+*/
+// 위의 코드는 잘 돌아가지만 마음에 들지 않으므로 다시 짰습니다.
+var groupAnagrams = function(strs) {
+    let dict = {};
+    for (let i = 0; i < strs.length; i++)
+    {
+        let tmp = strs[i].split('').sort();
+        if (!dict[tmp])
+        {
+            dict[tmp] = [];
+            dict[tmp].push(strs[i]);
+        }
+        else
+            dict[tmp].push(strs[i]);
+    }
+    let res = [];
+    for (let key in dict)
+    {
+        res.push(dict[key]);
     }
     return res;
 };
