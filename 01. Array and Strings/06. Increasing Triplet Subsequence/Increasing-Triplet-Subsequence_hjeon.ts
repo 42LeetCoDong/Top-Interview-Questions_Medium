454665fef7f826c4d20887ec289ac5c589f17652
hwano <jho2301@gmail.com>
2020-08-08 10:44:35 +0900

    Add files via upload

---
+++ 01. Array and Strings/06. Increasing Triplet Subsequence/Increasing-Triplet-Subsequence_hjeon.ts
@@ -0,0 +1,11 @@
+function increasingTriplet(nums: number[]): boolean {
+	let small: number = Number.MAX_VALUE;
+	let mid: number = Number.MAX_VALUE;
+
+	for (let num of nums) {
+		if (num < small) small = num;
+		else if (num < mid) mid = num;
+		else return (true)
+	}
+	return (false);
+};
