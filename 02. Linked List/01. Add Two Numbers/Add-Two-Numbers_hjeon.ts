f7ba9cffb499c488ad2823e9078f1e32022255a9
hwano <jho2301@gmail.com>
2020-08-09 15:24:11 +0900

    Update Add-Two-Numbers_hjeon.ts

--- 02. Linked List/01. Add Two Numbers/Add-Two-Numbers_hjeon.ts
+++ 02. Linked List/01. Add Two Numbers/Add-Two-Numbers_hjeon.ts
@@ -1,5 +1,3 @@
-
-
 function addTwoNumbers(l1: ListNode | null, l2: ListNode | null): ListNode | null {
 	let isRounding: boolean = false;
 	let l3: ListNode | null = new ListNode();
@@ -7,26 +5,27 @@ function addTwoNumbers(l1: ListNode | null, l2: ListNode | null): ListNode | nul
 
 	while (l1 || l2 || isRounding) {
         if (isRounding && nextL3) {
-		nextL3.val++;
-		isRounding = false;
-	}
-	if (l1 && l2 && nextL3) {
-		nextL3.val = l1.val + l2.val;	
-		if (9 < nextL3.val) {
-			nextL3.val -= 10;
-			isRounding = true;
+			nextL3.val++;
+			isRounding = false;
 		}
-	}
-	if (l1)
-		l1 = l1.next;
-	if (l2)
-		l2 = l2.next;
-	if (nextL3 && (l1 || l2 || isRounding))
-		nextL3.next = new ListNode();
-	else if (nextL3)
-		nextL3.next = null;
-	if (nextL3)
-		nextL3 = nextL3.next;
+        if (l1 && l2 && nextL3)
+            nextL3.val += l1.val + l2.val;	
+        else if (l1 && nextL3)
+            nextL3.val += l1.val;	
+        else if (l2 && nextL3)
+            nextL3.val += l2.val;	
+        if (nextL3 && 9 < nextL3.val) {
+            nextL3.val -= 10;
+            isRounding = true;
+        }
+		
+		if (l1) l1 = l1.next;
+		if (l2)	l2 = l2.next;
+		if (nextL3 && (l1 || l2 || isRounding))
+			nextL3.next = new ListNode();
+		else if (nextL3)
+			nextL3.next = null;
+		if (nextL3) nextL3 = nextL3.next;
 	}
 	return (l3);
 };
