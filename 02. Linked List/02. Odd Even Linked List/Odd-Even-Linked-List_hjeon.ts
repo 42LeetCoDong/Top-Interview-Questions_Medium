e55d7bc4c4f5c3466655219b85161ff1570ea020
hwano <jho2301@gmail.com>
2020-08-10 14:51:42 +0900

    Update Odd-Even-Linked-List_hjeon.ts

--- 02. Linked List/02. Odd Even Linked List/Odd-Even-Linked-List_hjeon.ts
+++ 02. Linked List/02. Odd Even Linked List/Odd-Even-Linked-List_hjeon.ts
@@ -1,14 +1,14 @@
 function oddEvenList(head) {
     if (!head) return head;
   
-    let odd = head;
-    let even = head.next;
-    while (odd.next && odd.next.next) {
-      let nextEven = odd.next;
-      odd.next = odd.next.next;
-      odd = odd.next;
-      nextEven.next = odd.next;
+    let oddTemp = head;
+    let evenHead = head.next;
+    while (oddTemp.next && oddTemp.next.next) {
+      let nextEven = oddTemp.next;
+      oddTemp.next = oddTemp.next.next;
+      oddTemp = oddTemp.next;
+      nextEven.next = oddTemp.next;
     }
-    odd.next = even;
+    oddTemp.next = evenHead;
     return head;
   }
