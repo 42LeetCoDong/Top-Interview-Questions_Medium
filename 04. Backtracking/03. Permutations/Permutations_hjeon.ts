471977ffc6547ce2c662b99103e6df860754c3a3
hwano <jho2301@gmail.com>
2020-08-20 10:44:26 +0900

    Add files via upload

---
+++ 04. Backtracking/03. Permutations/Permutations_hjeon.ts
@@ -0,0 +1,17 @@
+function permute(nums: number[]): number[][] {
+  const res: number[][] = [];
+  function recursive(nArr: number[], el: number[]) {
+    if (nArr.length === el.length) {
+        res.push(el);
+        return ;
+    }
+    for (let i = 0; i < nArr.length; i++)
+        if (!el.includes(nArr[i]))
+            recursive(nArr, [...el, nArr[i]]);
+  };
+  recursive(nums, []);
+  return (res);
+};
+
+
+console.log(permute([1,2,3]))
