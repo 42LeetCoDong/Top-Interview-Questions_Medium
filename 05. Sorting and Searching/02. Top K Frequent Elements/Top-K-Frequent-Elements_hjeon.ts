2d58eb163ab190a5fa0d14367a78ddf3fc0a1672
hwano <jho2301@gmail.com>
2020-08-24 09:24:33 +0900

    Update Top-K-Frequent-Elements_hjeon.ts

--- 05. Sorting and Searching/02. Top K Frequent Elements/Top-K-Frequent-Elements_hjeon.ts
+++ 05. Sorting and Searching/02. Top K Frequent Elements/Top-K-Frequent-Elements_hjeon.ts
@@ -2,13 +2,13 @@ function topKFrequent(nums: number[], k: number): number[] {
     const frequency: any = {};
     const res: number[] = [];
     
-    for (let num of nums) {
+    nums.forEach(num => {
         if (!frequency[num]) {
             frequency[num] = 1;
             res.push(num);
         }
         else frequency[num] += 1;
-    }
+    })  
     res.sort((a,b) => frequency[b] - frequency[a]);   
     return (res.filter(el => 0 < k--));
 };
