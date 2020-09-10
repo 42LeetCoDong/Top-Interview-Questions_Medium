3780ec71ae43d18f55a4d59d5c2d4fa970ffb91e
hwano <jho2301@gmail.com>
2020-09-10 10:23:43 +0900

    Update Sqrt(x)_hjeon.ts

--- 08. Math/05. Sqrt(x)/Sqrt(x)_hjeon.ts
+++ 08. Math/05. Sqrt(x)/Sqrt(x)_hjeon.ts
@@ -1,11 +1,15 @@
-function mySqrt(x: number): number {
-    let result: number = 1;
+// function mySqrt(x: number): number {
+//     let result: number = 1;
     
-    while (true) {
-        let pow: number = result ** 2;
+//     while (true) {
+//         let pow: number = result ** 2;
         
-        if (pow === x) return (result);
-        else if (x < pow) return (result - 1);
-        result++;
-    }
-};
+//         if (pow === x) return (result);
+//         else if (x < pow) return (result - 1);
+//         result++;
+//     }
+// };
+
+function mySqrt(x: number): number {
+    return (Math.floor(x ** 0.5));
+}
