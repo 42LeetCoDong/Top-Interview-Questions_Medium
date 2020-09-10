dc2ecf1f81ceb22b507e002595dd198aea884c4f
hwano <jho2301@gmail.com>
2020-09-10 10:21:35 +0900

    Create Sqrt(x)_hjeon.ts

---
+++ 08. Math/05. Sqrt(x)/Sqrt(x)_hjeon.ts
@@ -0,0 +1,11 @@
+function mySqrt(x: number): number {
+    let result: number = 1;
+    
+    while (true) {
+        let pow: number = result ** 2;
+        
+        if (pow === x) return (result);
+        else if (x < pow) return (result - 1);
+        result++;
+    }
+};
